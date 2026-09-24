485bd363f2a3a5f959f03fba27c1d5fd7d7ca5c8
M-Shees-Abbas <ma11212@st.habib.edu.pk>
2026-09-24 02:18:56 -0700

    lab4 some progress

---
+++ lab_4/task1(b).s
@@ -0,0 +1,23 @@
+    .text
+    .globl main
+
+main:
+    li     x10, 3
+    jal    x1, fact       # call fact(3)
+    j      end            # stop after return
+
+fact:
+    addi   x5 , x0 , 1    
+
+L1:
+    bge    x0 , x10 , exit #if (0 >= n), exit loop
+    mul    x5 , x5 , x10   
+    addi   x10 , x10 , -1  #n = n - 1
+    jal    x0 , L1         #repeat
+
+exit:
+    addi   x10 , x5 , 0    #return acc (move result into x10)
+    jalr   x0 , 0(x1)      #return
+
+end:
+    j      end
